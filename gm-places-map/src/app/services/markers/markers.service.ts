fbaa71ab11c4da9e75b5f75faab2fee48908b3d8
CelestialArcadia <22876185+CelestialArcadia@users.noreply.github.com>
2022-04-27 14:40:28 +0100

    Add: Models

--- gm-places-map/src/app/services/markers/markers.service.ts
+++ gm-places-map/src/app/services/markers/markers.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable } from "rxjs";
-import { Marker } from "src/app/models/marker.mode";
+import { Marker } from "src/app/models/marker.model";
 
 @Injectable({
   providedIn: 'root',
