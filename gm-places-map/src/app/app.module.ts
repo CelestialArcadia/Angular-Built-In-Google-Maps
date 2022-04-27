878ba555c0ce1cb15ae196be42bb1d943f041412
CelestialArcadia <22876185+CelestialArcadia@users.noreply.github.com>
2022-04-27 14:40:47 +0100

    Update: App

--- gm-places-map/src/app/app.module.ts
+++ gm-places-map/src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GmMapComponent } from './components/gm-map/gm-map.component';
+import { PlacesListComponent } from './components/places-list/places-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, GmMapComponent, PlacesListComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    PlacesViewModule,
+    MapViewModule,
+    InputViewModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
